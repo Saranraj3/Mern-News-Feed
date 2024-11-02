7bff1c154305fefb8e4b0c9a8ad973c045167186
Saranraj <www.saranrajcoder@gmail.com>
2024-11-02 09:58:27 +0530

    MongoDB Database Connected

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ require("dotenv").config();
 
 const app = express();
 
+// MongoDB Database Connection
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
   res.send("Hello Server");
 });
