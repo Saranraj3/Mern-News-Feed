55b9221c557ee3c3712f17db2532f48fe0ee7c9b
Saranraj <www.saranrajcoder@gmail.com>
2024-11-04 15:02:37 +0530

    App.jsx File Configured Authentication Path and Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import Home from './Pages/Home'
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
+import ProtectedRoute from "./Authentication/ProtectedRoute";
 
 const App = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+  const [authenticated, setAuthenticated] = useState(false);
 
-export default App
+  const login = () => {
+    setAuthenticated(true);
+  };
+
+  const signup = () => {
+    setAuthenticated(true);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login onlogin={login} />} />
+        <Route path="/signup" element={<Signup onsignup={signup} />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute
+              authenticated={authenticated}
+              component={<Home />}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
