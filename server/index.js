a582d3d3a046705e29ca4706d8298e262fb98b46
Saranraj <www.saranrajcoder@gmail.com>
2024-11-05 08:22:36 +0530

    API End Points Routes and Middleware Created index.js File

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import AuthenticationRoute from "./Routes/AuthenticationRoute.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 // MongoDB Database Connection
-
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
@@ -15,10 +18,29 @@ mongoose
     console.log(err);
   });
 
+// API Routes
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use("/auth", AuthenticationRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello Server");
 });
 
 app.listen(process.env.PORT, (req, res) => {
   console.log("Server Running");
+});
+
+// Middleware
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Something Went Wrong";
+  return res.status(statuscode).json({
+    success: false,
+    message,
+    statuscode,
+  });
 });
